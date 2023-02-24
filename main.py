c04e0707de608c8f4c09f8bb15d2e86e6bae5a3b
Relfick <relfick@yandex.ru>
2023-02-24 13:44:06 +0300

    add up command handler

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot
+from datetime import datetime
 import json
 import config
+import notion_api
 from telebot.types import *
 from Filters.IsAllowedUserFilter import IsAllowedUserFilter
 
@@ -24,6 +26,28 @@ def main():
     def send_welcome(message: Message):
         chat_id = message.chat.id
         bot.send_message(chat_id, 'hello boy')
+
+
+    @bot.message_handler(chats_and_admins=(allowed_chats, allowed_users), commands=['up'])
+    def post_up(up_msg: Message):
+        user_id = up_msg.from_user.id
+        try_msg = bot.reply_to(message=up_msg, text='Пытаюсь добавить...')
+        up_datetime = datetime.fromtimestamp(up_msg.date)
+
+        up_date = up_datetime.date()
+        up_time = up_datetime.time()
+
+        up_date = str(up_date)
+        up_time = round(up_time.hour + up_time.minute/60, 1)
+
+        success = notion_api.up(user_id, up_datetime)
+        if not success:
+            bot.delete_message(up_msg.chat.id, try_msg.id)
+            bot.reply_to(message=up_msg, text='Не удалось добавить!')
+        else:
+            bot.delete_message(up_msg.chat.id, try_msg.id)
+            bot.delete_message(up_msg.chat.id, up_msg.id)
+        print()
 
 
     @bot.message_handler(chats_and_admins=(allowed_chats, allowed_users), content_types=['text'])
