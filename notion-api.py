1064639845778114b695d72b2de95c806bca757d
Relfick <relfick@yandex.ru>
2023-02-24 13:27:12 +0300

    refactor

--- notion-api.py
+++ notion-api.py
@@ -4,7 +4,13 @@ import pandas as pd
 
 database_id = config.NOTION_DATABASE_ID
 notion_token = config.NOTION_TOKEN
-
+post_url = 'https://api.notion.com/v1/pages'
+post_headers = {
+        'accept': 'application/json',
+        'content-type': 'application/json',
+        'Authorization': f'Bearer {notion_token}',
+        'Notion-Version': '2022-06-28'
+    }
 default_date = '2030-01-01'
 
 properties = {
@@ -80,54 +86,14 @@ def get_sleep():
 
 
 def post_sleep():
-    url = f'https://api.notion.com/v1/pages'
+    props = properties.copy()
+
+
     newPageData = {
         'parent': {'database_id': database_id},
-        'properties': {
-            'Спал (МАКС)': {
-                'number': 5
-            },
-            'Встал в (РОМА)': {
-                'number': 5
-            },
-            'Лег в (МАКС)': {
-                'number': 5
-            },
-            'Комментарий': {
-                'rich_text': []
-            },
-            'БЕЗ МАТА (МАКСИМ)': {
-                'checkbox': True
-            },
-            'БЕЗ МАТА (РОМА)': {
-                'checkbox': True
-            },
-            'Встал в (МАКС)': {
-                'number': 5
-            },
-            'Лег в (РОМА)': {
-                'number': 5
-            },
-            'Спал (РОМА)': {
-                'number': 5
-            },
-            'Дата': {
-                'date': {
-                    'start': '2023-03-26',
-                    'end': None
-                }
-            },
-            'title': {
-                'title': []
-            }
-        }
+        'properties': props
     }
 
-    r = requests.post(url, headers={
-        'accept': 'application/json',
-        'content-type': 'application/json',
-        'Authorization': f'Bearer {notion_token}',
-        'Notion-Version': '2022-06-28'
-    }, json=newPageData)
+    r = requests.post(url=post_url, headers=post_headers, json=newPageData)
 
     return r.status_code
