8c0da07f735c2a81c1ac166346fe9889e3209cce
Relfick <relfick@yandex.ru>
2023-02-24 15:07:57 +0300

    post new sleep-row added

--- notion_api.py
+++ notion_api.py
@@ -1,7 +1,10 @@
 import requests
 import config
+from datetime import datetime, timedelta
 import pandas as pd
 
+allowed_users_dict = config.ALLOWED_USERS_DICT
+properties = config.TABLE_PROPERTIES
 database_id = config.NOTION_DATABASE_ID
 notion_token = config.NOTION_TOKEN
 post_url = 'https://api.notion.com/v1/pages'
@@ -12,45 +15,6 @@ post_headers = {
         'Notion-Version': '2022-06-28'
     }
 default_date = '2030-01-01'
-
-properties = {
-    'Спал (МАКС)': {
-        'number': None
-    },
-    'Встал в (РОМА)': {
-        'number': None
-    },
-    'Лег в (МАКС)': {
-        'number': None
-    },
-    'Комментарий': {
-        'rich_text': []
-    },
-    'БЕЗ МАТА (МАКСИМ)': {
-        'checkbox': False
-    },
-    'БЕЗ МАТА (РОМА)': {
-        'checkbox': False
-    },
-    'Встал в (МАКС)': {
-        'number': None
-    },
-    'Лег в (РОМА)': {
-        'number': None
-    },
-    'Спал (РОМА)': {
-        'number': None
-    },
-    'Дата': {
-        'date': {
-            'start': default_date,
-            'end': None
-        }
-    },
-    'title': {
-        'title': []
-    }
-}
 
 
 def get_sleep():
@@ -82,13 +46,10 @@ def get_sleep():
 
     # df = pd.DataFrame.from_dict(data=data)
 
-    print(result)
+    return data
 
 
-def post_sleep():
-    props = properties.copy()
-
-
+def post_sleep(props):
     newPageData = {
         'parent': {'database_id': database_id},
         'properties': props
@@ -96,7 +57,47 @@ def post_sleep():
 
     r = requests.post(url=post_url, headers=post_headers, json=newPageData)
 
-    return r.status_code
+    return r.status_code == 200
 
 
-def up()
+def get_last_down_time(user: str, up_date: str) -> int:
+    sleep = get_sleep()
+    down_date_last = str(datetime.strptime(up_date, '%Y-%m-%d').date() - timedelta(days=1))
+    down_time_last = -1
+    for row in sleep:
+        if row['Дата'] == down_date_last:
+            down_time_last = row[f'Лег в ({user})']
+            break
+    # TODO: если вчера не записал
+    return down_time_last
+
+
+def create_up_props(user: str, up_date: str, up_time: float):
+    props = properties.copy()
+    down_time_last = get_last_down_time(user, up_date)
+    sleep_hours = 24-down_time_last+up_time
+
+    props[f'Встал в ({user})']['number'] = up_time
+    props['Дата']['date']['start'] = up_date
+    props[f'Спал ({user})']['number'] = sleep_hours
+
+    return props
+
+
+
+def up(user_id: int, up_datetime: datetime):
+    up_date = up_datetime.date()
+    up_time = up_datetime.time()
+
+    up_date = str(up_date)
+    up_time = round(up_time.hour + up_time.minute / 60, 1)
+
+    user = ''
+    for k, v in allowed_users_dict.items():
+        if user_id in v:
+            user = k
+            break
+
+    props = create_up_props(user, up_date, up_time)
+    success = post_sleep(props)
+    return success
