f6dc5288b8ba535ca3dcd993ce8ec05d1938a0bb
Relfick <relfick@yandex.ru>
2023-02-25 14:07:52 +0300

    update last sleep row added (when caller woke up the second)

--- notion_api.py
+++ notion_api.py
@@ -7,8 +7,8 @@ allowed_users_dict = config.ALLOWED_USERS_DICT
 properties = config.TABLE_PROPERTIES
 database_id = config.NOTION_DATABASE_ID
 notion_token = config.NOTION_TOKEN
-post_url = 'https://api.notion.com/v1/pages'
-post_headers = {
+base_url = 'https://api.notion.com/v1/pages'
+base_headers = {
         'accept': 'application/json',
         'content-type': 'application/json',
         'Authorization': f'Bearer {notion_token}',
@@ -35,57 +35,63 @@ def get_sleep():
     r = requests.post(url, headers=headers)
 
     result = r.json()['results']
-    rows = [row['properties'] for row in result]
-    data = []
-    for row in rows:
-        row_dict = {}
-        row_data = list(row.items())
-        for prop in row_data:
-            row_dict[prop[0]] = get_row_value(prop[1])
-        data.append(row_dict)
+    # rows = [row['properties'] for row in result]
+    # data = []
+    # for row in rows:
+    #     row_dict = {}
+    #     row_data = list(row.items())
+    #     for prop in row_data:
+    #         row_dict[prop[0]] = get_row_value(prop[1])
+    #     data.append(row_dict)
+    #
+    # # df = pd.DataFrame.from_dict(data=data)
+    # return data
 
-    # df = pd.DataFrame.from_dict(data=data)
+    return result
 
-    return data
-
-
-def post_sleep(props):
-    newPageData = {
-        'parent': {'database_id': database_id},
-        'properties': props
-    }
-
-    r = requests.post(url=post_url, headers=post_headers, json=newPageData)
-
-    return r.status_code == 200
-
-
-def get_last_down_time(user: str, up_date: str) -> int:
-    sleep = get_sleep()
-    down_date_last = str(datetime.strptime(up_date, '%Y-%m-%d').date() - timedelta(days=1))
-    down_time_last = -1
-    for row in sleep:
-        if row['Дата'] == down_date_last:
-            down_time_last = row[f'Лег в ({user})']
-            break
-    # TODO: если вчера не записал
-    return down_time_last
-
-
-def create_up_props(user: str, up_date: str, up_time: float):
+def get_new_up_props(last_row, user, up_date, up_time):
     props = properties.copy()
-    down_time_last = get_last_down_time(user, up_date)
-    sleep_hours = 24-down_time_last+up_time
+
+    # TODO: down_time_last может быть None
+    down_time_last = last_row['properties'][f'Лег в ({user})']['number']
+    sleep_hours = 24 - down_time_last + up_time
 
     props[f'Встал в ({user})']['number'] = up_time
     props['Дата']['date']['start'] = up_date
     props[f'Спал ({user})']['number'] = sleep_hours
 
-    return props
+    new_page_data = {
+        'parent': {'database_id': database_id},
+        'properties': props
+    }
 
+    return new_page_data
+
+
+def get_updated_up_props(last_2_rows, user, up_date, up_time):
+    props = last_2_rows[0]['properties']
+
+    # TODO: down_time_last может быть None
+    down_time_last = last_2_rows[1]['properties'][f'Лег в ({user})']['number']
+    sleep_hours = 24 - down_time_last + up_time
+
+    props[f'Встал в ({user})']['number'] = up_time
+    props['Дата']['date']['start'] = up_date
+    props[f'Спал ({user})']['number'] = sleep_hours
+
+    updated_page_data = {
+        'properties': props
+    }
+
+    return updated_page_data
 
 
 def up(user_id: int, up_datetime: datetime):
+    method = ''
+    url = ''
+    payload = ''
+    headers = base_headers
+
     up_date = up_datetime.date()
     up_time = up_datetime.time()
 
@@ -93,11 +99,32 @@ def up(user_id: int, up_datetime: datetime):
     up_time = round(up_time.hour + up_time.minute / 60, 1)
 
     user = ''
-    for k, v in allowed_users_dict.items():
-        if user_id in v:
-            user = k
+    for username, ids in allowed_users_dict.items():
+        if user_id in ids:
+            user = username
             break
 
-    props = create_up_props(user, up_date, up_time)
-    success = post_sleep(props)
-    return success
+    last_2_rows = get_sleep()[:2]
+    last_row_date = last_2_rows[0]['properties']['Дата']['date']['start']
+
+    if last_row_date == up_date:
+        # Юзер проснулся вторым
+        payload = get_updated_up_props(last_2_rows, user, up_date, up_time)
+
+        row_id = last_2_rows[0]['id']
+        url = base_url + f'/{row_id}'
+        method = 'PATCH'
+    elif last_row_date == str(datetime.strptime(up_date, '%Y-%m-%d').date() - timedelta(days=1)):
+        # Юзер проснулся первым
+        payload = get_new_up_props(last_2_rows[0], user, up_date, up_time)
+
+        url = base_url
+        method = 'POST'
+    else:
+        # Ошибка. Последняя дата - не сегодня и не вчера, значит бота давно не юзали
+        return False
+
+
+    result = requests.request(method=method, url=url, headers=headers, json=payload)
+
+    return result.status_code == 200
